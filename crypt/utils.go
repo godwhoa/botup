93b18fcdf7fed5fb64f0e6b11ab5ea6e29e002fd
gjdaniel1999 <gjdaniel1999@gmail.com>
2016-12-27 19:38:00 +0530

    Add crypt

---
+++ crypt/utils.go
@@ -0,0 +1,39 @@
+package crypt
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"github.com/minio/blake2b-simd"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func RandomBytes() []byte {
+	b := make([]byte, 2)
+	rand.Read(b)
+	return b
+}
+
+func UID(user string) (string, error) {
+	hash, err := blake2b.New(&blake2b.Config{Size: 16})
+	if err != nil {
+		return "", err
+	}
+	b := []byte(user)
+	hash.Write(b)
+	hash.Write(RandomBytes())
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+func Verify(hashed string, pass string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashed), []byte(pass))
+	return err == nil
+}
+
+func Hash(pass string) (string, error) {
+	// DefaultCost = 10
+	hashed, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
+}
