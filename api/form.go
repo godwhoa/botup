228d5dadf781dbeca620dd091b9e1250d5dfb5ef
gjdaniel1999 <gjdaniel1999@gmail.com>
2017-01-03 15:02:00 +0530

    Fix form

--- api/form.go
+++ api/form.go
@@ -1,10 +1,11 @@
 package api
 
 import (
-	"github.com/godwhoa/random-shit/botup.me/botup"
-	"github.com/gorilla/sessions"
 	"net/http"
 	"strconv"
+
+	"github.com/godwhoa/random-shit/botup.me/botup"
+	"github.com/gorilla/sessions"
 )
 
 // User form validation
@@ -20,7 +21,7 @@ func validate_reg_form(r *http.Request) (botup.User, error) {
 
 // Bot form validation
 func validate_addbot_form(r *http.Request, store *sessions.CookieStore) (botup.Bot, error) {
-	session, _ := b.Store.Get(r, "login")
+	session, _ := store.Get(r, "login")
 
 	addr := r.FormValue("addr")
 	channel := r.FormValue("channel")
@@ -29,7 +30,7 @@ func validate_addbot_form(r *http.Request, store *sessions.CookieStore) (botup.B
 	}
 
 	bot := botup.Bot{}
-	bot.UID = session.Values["uid"]
+	bot.UID = session.Values["uid"].(string)
 	bot.Addr = addr
 	bot.Channel = channel
 
@@ -37,47 +38,47 @@ func validate_addbot_form(r *http.Request, store *sessions.CookieStore) (botup.B
 }
 
 func validate_addplugin_form(r *http.Request, store *sessions.CookieStore) (botup.Plugin, error) {
-	session, _ := b.Store.Get(r, "login")
+	session, _ := store.Get(r, "login")
 
 	plugin := botup.Plugin{}
 
 	bid, err := strconv.Atoi(r.FormValue("bid"))
 	plugin_ := r.FormValue("plugin")
 
-	if bid == "" || plugin_ == "" || err != nil {
+	if plugin_ == "" || err != nil {
 		return plugin, invalidForm
 	}
 
 	plugin.BID = bid
-	plugin.UID = session.Values["uid"]
+	plugin.UID = session.Values["uid"].(string)
 	plugin.Plugin = plugin_
 
 	return plugin, nil
 }
 
 func validate_removebot_form(r *http.Request, store *sessions.CookieStore) (botup.Bot, error) {
-	session, _ := b.Store.Get(r, "login")
+	session, _ := store.Get(r, "login")
 
 	bid, err := strconv.Atoi(r.FormValue("bid"))
-	if bid == "" || err != nil {
+	if err != nil {
 		return botup.Bot{}, invalidForm
 	}
 
 	bot := botup.Bot{}
-	bot.UID = session.Values["uid"]
+	bot.UID = session.Values["uid"].(string)
 	bot.BID = bid
 
 	return bot, nil
 }
 
 func validate_removeplugin_form(r *http.Request, store *sessions.CookieStore) (botup.Plugin, error) {
-	session, _ := b.Store.Get(r, "login")
+	session, _ := store.Get(r, "login")
 
 	bid, err := strconv.Atoi(r.FormValue("bid"))
 	plugin := r.FormValue("plugin")
-	if bid == "" || plugin == "" || err != nil {
+	if plugin == "" || err != nil {
 		return botup.Plugin{}, invalidForm
 	}
 
-	return botup.Plugin{bid, session.Values["uid"], plugin}, nil
+	return botup.Plugin{bid, session.Values["uid"].(string), plugin}, nil
 }
