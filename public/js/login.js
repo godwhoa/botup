d8e68f0cc7adb887f02a7f52b1619f10a5b82a33
gjdaniel1999 <gjdaniel1999@gmail.com>
2017-01-14 19:55:06 +0530

    Add api interaction for login

---
+++ public/js/login.js
@@ -0,0 +1,25 @@
+function SendForm() {
+	var email = $('#email').val();
+	var password = $('#password').val();
+	$.post('/api/user/login', {'email': email,'pass':password}, function(response){
+		$('.notification').css('display','none');
+		switch (response){
+			case "OK_LOGGED_IN":
+				$('#ok-login').css('display','block');
+				setTimeout(function(){
+					window.location.pathname = "/dashboard"
+				},1000)
+			case "ERR_WRONG_CREDENTIALS":
+				$('#wrong-cred').css('display','block');
+			case "ERR_INTERNAL":
+				$('#inter-err').css('display','block');
+			default:
+				$('#inter-err').css('display','block');
+		}
+		setTimeout(function(){
+			$('.notification').css('display','none');
+		},2000)
+	});
+}
+
+$('#login').on('click',SendForm);
