b5dfc8ce5f266ae942c4409b2c5d44fbbdb90de6
gjdaniel1999 <gjdaniel1999@gmail.com>
2017-01-15 15:38:49 +0530

    Minor login fixes

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -3,16 +3,20 @@ function SendForm() {
 	var password = $('#password').val();
 	$.post('/api/user/login', {'email': email,'pass':password}, function(response){
 		$('.notification').css('display','none');
+		console.log(response)
 		switch (response){
 			case "OK_LOGGED_IN":
 				$('#ok-login').css('display','block');
 				setTimeout(function(){
-					// window.location.pathname = "/dashboard"
+					window.location.pathname = "/dashboard"
 				},1000)
+				break;
 			case "ERR_WRONG_CREDENTIALS":
 				$('#wrong-cred').css('display','block');
+				break;
 			case "ERR_INTERNAL":
 				$('#inter-err').css('display','block');
+				break;
 			default:
 				$('#inter-err').css('display','block');
 		}
